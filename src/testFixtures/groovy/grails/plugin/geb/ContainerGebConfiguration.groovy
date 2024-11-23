7f5593333aceea2f0f174f46b9f3a3aee4fb04c1
James Daugherty <jdaugherty@jdresources.net>
2024-11-23 10:23:20 -0500

    cleanup - compilestatic, license, and documentation

--- src/testFixtures/groovy/grails/plugin/geb/ContainerGebConfiguration.groovy
+++ src/testFixtures/groovy/grails/plugin/geb/ContainerGebConfiguration.groovy
@@ -1,11 +1,33 @@
+/*
+ * Copyright 2024 original author or authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package grails.plugin.geb
 
 import groovy.transform.CompileStatic
 import groovy.transform.Memoized
+import groovy.transform.PackageScope
 import groovy.util.logging.Slf4j
 import org.testcontainers.containers.BrowserWebDriverContainer
 import org.testcontainers.containers.VncRecordingContainer
 
+/**
+ * A container class to parse the configuration used by {@link grails.plugin.geb.ContainerGebRecordingExtension}
+ *
+ * @author James Daugherty
+ * @since 5.0.0
+ */
 @Slf4j
 @CompileStatic
 class ContainerGebConfiguration {
--- src/testFixtures/groovy/grails/plugin/geb/ContainerGebRecordingExtension.groovy
+++ src/testFixtures/groovy/grails/plugin/geb/ContainerGebRecordingExtension.groovy
@@ -1,5 +1,21 @@
+/*
+ * Copyright 2024 original author or authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package grails.plugin.geb
 
+import groovy.transform.CompileStatic
 import groovy.transform.TailRecursive
 import groovy.util.logging.Slf4j
 import org.spockframework.runtime.extension.IGlobalExtension
@@ -7,7 +23,14 @@ import org.spockframework.runtime.model.SpecInfo
 
 import java.time.LocalDateTime
 
+/**
+ * A Spock Extension that manages the Testcontainers lifecycle for a {@link grails.plugin.geb.ContainerGebSpec}
+ *
+ * @author James Daugherty
+ * @since 5.0.0
+ */
 @Slf4j
+@CompileStatic
 class ContainerGebRecordingExtension implements IGlobalExtension {
     ContainerGebConfiguration configuration
 
